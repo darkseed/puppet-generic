83170d4b1b90e67a2a28c939d2c892b893687454
Bart Cortooms <bart@kumina.nl>
2008-11-06 11:27:57 +0100

    Use config file layout as used by the Smokeping 2.3 Debian package.
    darcs-hash:20081106102757-431e1-ea06d31621cdc372d266e753e75794099b2308aa.gz

--- smokeping/manifests/init.pp
+++ smokeping/manifests/init.pp
@@ -9,11 +9,30 @@ class smokeping {
 		hasrestart => true,
 	}
 
-	file { "/etc/smokeping/config":
-		source => "puppet://puppet/smokeping/config",
-		owner => "root",
-		group => "root",
-		mode => 644,
-		notify => Service["smokeping"],
+	file {
+		"/etc/smokeping/config.d":
+			source => "puppet://puppet/smokeping/config.d",
+			owner => "root",
+			group => "root",
+			recurse => true,
+			notify => Service["smokeping"];
+		"/etc/smokeping/basepage.html":
+			source => "puppet://puppet/smokeping/basepage.html",
+			mode => 644,
+			owner => "root",
+			group => "root",
+			notify => Service["smokeping"];
+		"/etc/smokeping/smokemail":
+			source => "puppet://puppet/smokeping/smokemail",
+			mode => 644,
+			owner => "root",
+			group => "root",
+			notify => Service["smokeping"];
+		"/etc/smokeping/tmail":
+			source => "puppet://puppet/smokeping/tmail",
+			mode => 644,
+			owner => "root",
+			group => "root",
+			require => Package["smokeping"];
 	}
 }

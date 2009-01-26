689178c8c97554cd0db597592e478c103aaf2818
Kees Meijs <kees@kumina.nl>
2009-01-26 09:13:08 +0100

    Only create /etc/ssl/Makefile on CA's
    darcs-hash:20090126081308-6c6a4-cdd48ba329460b5fe0bc28e32c6d4d2fb33029e5.gz

--- openssl/manifests/init.pp
+++ openssl/manifests/init.pp
@@ -2,14 +2,6 @@ class openssl::common {
 	package { ["openssl"]:
 		ensure => installed,
 	}
-
-	file { "/etc/ssl/Makefile":
-		source => "puppet://puppet/openssl/Makefile",
-		mode => 644,
-		owner => "root",
-		group => "root",
-		require => Package["openssl"];
-	}
 }
 
 class openssl::server {
@@ -30,6 +22,12 @@ class openssl::ca {
 		"/etc/ssl/requests":
 			ensure => directory,
 			mode => 750,
+			owner => "root",
+			group => "root",
+			require => Package["openssl"];
+		"/etc/ssl/Makefile":
+			source => "puppet://puppet/openssl/Makefile",
+			mode => 644,
 			owner => "root",
 			group => "root",
 			require => Package["openssl"];

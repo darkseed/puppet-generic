ef170fa9999d8b12757adfdb091f45804ddb3160
Kees Meijs <kees@kumina.nl>
2009-02-02 10:28:48 +0100

    Move nagios::nsca to separate file
    darcs-hash:20090202092848-6c6a4-2f7d7397d48ebcb9e8183ba8bd640850690d13ed.gz

--- nagios/manifests/server.pp
+++ nagios/manifests/server.pp
@@ -1,23 +1,3 @@
-class nagios::nsca {
-	package { "nsca":
-		ensure => installed,
-	}
-
-	service { "nsca":
-		enable => true,
-		ensure => running,
-		hasrestart => true,
-		subscribe => File["/etc/nsca.cfg"],
-	}
-
-	file { "/etc/nsca.cfg":
-		source => "puppet://puppet/nagios/nsca/nsca.cfg",
-		mode => 640,
-		owner => "root",
-		group => "nagios",
-	}
-}
-
 class nagios::server {
 	include nagios::plugins
 	include nagios::nsca

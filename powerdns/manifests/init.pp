542588ee0c3512718f8d81c1ed7af100d6aaea2b
Kees Meijs <kees@kumina.nl>
2009-02-11 12:02:00 +0100

    PowerDNS package installation
    darcs-hash:20090211110200-6c6a4-ae18214afeb89530a4648672196ae912e5fa8aa0.gz

--- powerdns/manifests/init.pp
+++ powerdns/manifests/init.pp
@@ -1,8 +1,82 @@
+# Puppet manifest for PowerDNS, tested on Debian GNU/Linux 4.0 (etch).
+#
+# Copyright (c) 2009 by Kees Meijs <kees@kumina.nl> for Kumina bv.
+#
+# This work is licensed under the Creative Commons Attribution-Share Alike 3.0
+# Unported license. In short: you are free to share and to make derivatives of
+# this work under the conditions that you appropriately attribute it, and that
+# you only distribute it under the same, similar or a compatible license. Any
+# of the above conditions can be waived if you get permission from the copyright
+# holder.
+
+class powerdns::common {
+	# Make sure /var/lib/powerdns has correct permissions.
+	file {
+		"/var/lib/powerdns":
+			ensure => directory,
+			owner => "root",
+			group => "pdns",
+			mode => 775,
+			require => Package["pdns-server"];
+	}
+
+	# Make sure PowerDNS is running.
+	service {
+		"pdns":
+			ensure => running,
+			hasrestart => true,
+			require => File["/var/lib/powerdns"];
+	}
+}
+
 class powerdns::master {
+	include powerdns::common
+
+	# Install needed packages.
+        package {
+		"pdns-server":
+			ensure => installed;
+		"pdns-backend-mysql":
+			ensure => installed;
+	}
+
+	# TODO
+	# /etc/powerdns/pdns.conf
+	# /etc/powerdns/pdns.d/(pdns.local)
+
 }
 
 class powerdns::slave {
+	include powerdns::common
+
+	# Install needed packages.
+        package {
+		"pdns-server":
+			ensure => installed;
+		"pdns-backend-sqlite":
+			ensure => installed;
+	}
+
+	# TODO
+	# /etc/powerdns/pdns.conf
+	# /etc/powerdns/pdns.d/(pdns.local)
 }
 
 class powerdns::recursor {
+	# Install needed packages.
+        package {
+		"pdns-recursor":
+			ensure => installed;
+	}
+
+	# Make sure PowerDNS recursor is running.
+	service {
+		"pdns-recursor":
+			ensure => running,
+			hasrestart => true,
+			require => Package["pdns-recursor"];
+	}
+
+	# TODO
+	# /etc/powerdns/recursor.conf
 }

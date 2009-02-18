1eb059fef9e8c2dd713af2f48431e89081df75e2
Kees Meijs <kees@kumina.nl>
2009-02-18 10:03:07 +0100

    Use other Xen packages for Lenny on dom0's
    darcs-hash:20090218090307-6c6a4-a46f8f57d2bcf9efb6a05c91aefef9a22834e511.gz

--- xen/manifests/init.pp
+++ xen/manifests/init.pp
@@ -1,19 +1,12 @@
 class xen::dom0 {
+	include "xen::dom0::$lsbdistcodename"
+
 	# Architecture independent packages
 	package { ["xen-utils-common", "xen-tools", "bridge-utils"]:
 		ensure => installed,
 	}
 
-	# Architecture dependent packages
-	$archdependent = $architecture ? {
-		i386  => ["xen-hypervisor-3.0.3-1-i386-pae", "linux-image-2.6-xen-686"],
-		amd64 => ["xen-hypervisor-3.0.3-1-amd64", "linux-image-2.6-xen-amd64"],
-	}
-
-	package { $archdependent:
-		ensure => installed,
-	}
-
+	# CHECK IF THIS IS COMPATIBLE WITH LENNY
 	file { "/etc/default/xendomains":
 		source => "puppet://puppet/xen/dom0/default/xendomains",
 		owner => "root",
@@ -21,6 +14,8 @@ class xen::dom0 {
 		mode => 644,
 		require => Package["xen-utils-common"];
 	}
+
+	# OTHER XEN CONFIGURATION?
 
 	# Create directories (missing by default)
 	file {
@@ -36,5 +31,29 @@ class xen::dom0 {
 			group => "root",
 			mode => 755,
 			require => Package["xen-utils-common"];
+	}
+}
+
+class xen::dom0::etch {
+	$archdependent = $architecture ? {
+		i386  => ["xen-hypervisor-3.0.3-1-i386-pae", "linux-image-2.6-xen-686"],
+		amd64 => ["xen-hypervisor-3.0.3-1-amd64", "linux-image-2.6-xen-amd64"],
+	}
+	
+	# Architecture dependent packages
+	package { $archdependent:
+		ensure => installed,
+	}
+}
+
+class xen::dom0::lenny {
+	$archdependent = $architecture ? {
+		i386  => ["xen-hypervisor-3.2-1-i386", "linux-image-2.6-xen-686"],
+		amd64 => ["xen-hypervisor-3.2-1-amd64", "linux-image-2.6-xen-amd64"],
+	}
+	
+	# Architecture dependent packages
+	package { $archdependent:
+		ensure => installed,
 	}
 }

d13837d9ed2c02196070369c018722c094dc46e9
Kees Meijs <kees@kumina.nl>
2009-02-27 12:55:01 +0100

    Default configuration for dovecot
    darcs-hash:20090227115501-6c6a4-d0a13f86fd62161f14b0a3489bbc9209abe8c2fb.gz

--- dovecot/manifests/init.pp
+++ dovecot/manifests/init.pp
@@ -9,6 +9,27 @@ class dovecot::common {
 			ensure => running,
 			require => Package["dovecot-common"],
 	}
+
+	file {
+		"/etc/dovecot/dovecot.conf":
+			owner => "root",
+			group => "root",
+			source => "puppet://puppet/dovecot/dovecot/dovecot.conf",
+			mode => 644,
+			notify => Service["dovecot"];
+		"/etc/dovecot/dovecot-ldap.conf":
+			owner => "root",
+			group => "root",
+			source => "puppet://puppet/dovecot/dovecot/dovecot-ldap.conf",
+			mode => 600,
+			notify => Service["dovecot"];
+		"/etc/dovecot/dovecot-sql.conf":
+			owner => "root",
+			group => "root",
+			source => "puppet://puppet/dovecot/dovecot/dovecot-sql.conf",
+			mode => 600,
+			notify => Service["dovecot"];
+	}
 }
 
 class dovecot::imap inherits dovecot::common {

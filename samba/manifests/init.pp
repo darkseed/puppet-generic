509b385cfa83ac0f52da60c3578f95516a3da253
Kees Meijs <kees@kumina.nl>
2009-03-31 10:23:05 +0200

    Finish up basic Samba module
    darcs-hash:20090331082305-6c6a4-e3b1e645befa34cb29567727083bed90a809aa63.gz

--- samba/manifests/init.pp
+++ samba/manifests/init.pp
@@ -1,2 +1,26 @@
+class samba::common {
+	package {
+		"samba":
+			ensure => installed;
+	}
+}
+
 class samba::server {
+	include samba::common
+
+	service {
+		"samba":
+			subscribe => File["/etc/samba/smb.conf"],
+			ensure => running;
+			# TODO: FIX STATUS (Using Tim's patch?)
+	}
+
+	file {
+		"/etc/samba/smb.conf":
+			source => "puppet://puppet/samba/smb.conf",
+			mode => 644,
+			owner => "root",
+			group => "root",
+			require => Package["samba"];
+	}
 }

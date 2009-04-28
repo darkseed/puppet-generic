4829f7c7ddf378557a5585f57762c8a0c17c7908
Bart Cortooms <bart@kumina.nl>
2009-04-28 16:16:11 +0200

    The default path names for the log file and the IP address lease file are completely non-FHS compliant. Fix that.
    darcs-hash:20090428141611-431e1-54c6b7500c71a224b6cd2e6c00c7b49723f2fd96.gz

--- openvpn/manifests/init.pp
+++ openvpn/manifests/init.pp
@@ -1,7 +1,6 @@
 class openvpn::common {
-	package {
-		"openvpn":
-			ensure => installed,
+	package { "openvpn":
+		ensure => installed,
 	}
 }
 
@@ -14,25 +13,29 @@ class openvpn::server {
 			mode => 644,
 			owner => "root",
 			group => "root",
-			require => Package["openvpn"],
+			require => [Package["openvpn"], File["/var/lib/openvpn"]];
+		"/var/lib/openvpn":
+			ensure => "directory",
+			owner => "root",
+			group => "root",
+			mode => 750;
 	}
 
-	service {
-		"openvpn":
-			subscribe => File["/etc/openvpn/server.conf"],
-			ensure => running,
+	service { "openvpn":
+		subscribe => File["/etc/openvpn/server.conf"],
+		ensure => running,
 	}
+
 }
 
 class openvpn::client {
 	include openvpn::common
 
-	file {
-		"/etc/openvpn/client.conf":
-			source => "puppet://puppet/openvpn/client.conf",
-			mode => 644,
-			owner => "root",
-			group => "root",
-			require => Package["openvpn"],
+	file { "/etc/openvpn/client.conf":
+		source => "puppet://puppet/openvpn/client.conf",
+		mode => 644,
+		owner => "root",
+		group => "root",
+		require => Package["openvpn"],
 	}
 }

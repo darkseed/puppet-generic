6e5d0253510344e1816c595efde6d566aa6095e8
Bart Cortooms <bart@kumina.nl>
2009-05-06 14:13:57 +0200

    Module for PHP.
    darcs-hash:20090506121357-431e1-064228b4bdff46970baf6cc78f29d3b16f6a72aa.gz

---
+++ php/manifests/init.pp
@@ -0,0 +1,66 @@
+class php::common {
+	file { "/etc/php$phpversion/conf.d/security.ini":
+		owner => "root",
+		group => "root",
+		mode => 644,
+		source => "puppet://puppet/php/shared/conf.d/security.ini",
+	}
+}
+
+class php::php5::common {
+	$phpversion = 5
+	include php::common
+}
+
+class php::php5::modphp {
+	include php::php5::common
+
+	package { "libapache2-mod-php5":
+		ensure => installed,
+	}
+}
+
+class php::php5::cgi {
+	include php::php5::common
+
+	package { "php5-cgi":
+		ensure => installed,
+	}
+}
+
+class php::php5::cli {
+	include php::php5::common
+
+	package { "php5-cgi":
+		ensure => installed,
+	}
+}
+
+class php::php4::common {
+	$phpversion = 4
+	include php::common
+}
+
+class php::php4::modphp {
+	include php::php4::common
+
+	package { "libapache2-mod-php4":
+		ensure => installed,
+	}
+}
+
+class php::php4::cgi {
+	include php::php4::common
+
+	package { "php4-cgi":
+		ensure => installed,
+	}
+}
+
+class php::php4::cli {
+	include php::php4::common
+
+	package { "php4-cli":
+		ensure => installed,
+	}
+}

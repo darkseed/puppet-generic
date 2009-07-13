285d8962b83f05fd5e0c1ab061bd373d80815866
Bart Cortooms <bart@kumina.nl>
2009-07-13 15:41:09 +0200

    Make compatible with Lenny.
    darcs-hash:20090713134109-431e1-06c5c9d862b8ab6d6e4d7726edc7d7e42f0ecbbd.gz

--- openldap/manifests/init.pp
+++ openldap/manifests/init.pp
@@ -3,6 +3,12 @@ class openldap::client {
 		ensure => installed,
 	}
 
+	# Need to move this to a separate modules - it conflicts with the bind
+	# modules which also needs make.
+	package { "make":
+		ensure => installed,
+	}
+
 	$libldap = $lsbdistcodename ? {
 		lenny => "libldap-2.4-2",
 		default => "libldap2",
@@ -85,13 +91,13 @@ class openldap::client {
 
 class openldap::server {
 	define ldap::server::proxy_ad($ad_server_uri, $suffix) {
-		file { "/etc/ldap/slapd.conf.d/proxy_ad_$name.conf":
+		file { "/etc/ldap/slapd.db.d/proxy_ad_$name.conf":
 			owner => "root",
 			group => "root",
 			mode => 640,
 			content => template("openldap/server/proxy_ad.conf"),
 			notify => Exec["update-slapd-conf"],
-			require => File["/etc/ldap/slapd.conf.d"],
+			require => File["/etc/ldap/slapd.db.d"],
 		}
 	}
 
@@ -103,13 +109,13 @@ class openldap::server {
 			$database_dir = "/var/lib/ldap/$name"
 		}
 
-		file { "/etc/ldap/slapd.conf.d/database_$name.conf":
-			owner => "root",
+		file { "/etc/ldap/slapd.db.d/$name.conf":
+			owner => "openldap",
 			group => "root",
 			mode => 640,
 			content => template("openldap/server/database.conf"),
 			notify => Exec["update-slapd-conf"],
-			require => File["/etc/ldap/slapd.conf.d"],
+			require => File["/etc/ldap/slapd.db.d"],
 		}
 
 		file { "$database_dir":
@@ -133,39 +139,72 @@ class openldap::server {
 
 	file {
 		"/etc/ldap/slapd.conf":
-			source => "puppet://puppet/openldap/server/slapd.conf",
-			owner => "root",
+			source => ["puppet://puppet/openldap/server/$lsbdistcodename/slapd.conf", "puppet://puppet/openldap/server/slapd.conf"],
+			owner => "openldap",
 			group => "root",
 			mode => 640,
 			require => Package["slapd"];
-		"/etc/ldap/slapd.conf.d":
+		"/etc/ldap/slapd.main.d":
 			ensure => directory,
-			owner => "root",
+			owner => "openldap",
 			group => "root",
 			mode => 750,
 			require => Package["slapd"];
-		"/etc/ldap/slapd.conf.d/_dummy":
+		"/etc/ldap/slapd.main.d/_dummy":
 			ensure => file,
-			owner => "root",
+			owner => "openldap",
 			group => "root",
 			content => "# Needed for the Makefile, to ensure there's at least 1 file here. Do not remove.\n",
-			require => File["/etc/ldap/slapd.conf.d"];
+			require => File["/etc/ldap/slapd.db.d"];
+		"/etc/ldap/slapd.backend.d":
+			ensure => directory,
+			owner => "openldap",
+			group => "root",
+			mode => 750,
+			require => Package["slapd"];
+		"/etc/ldap/slapd.backend.d/_dummy":
+			ensure => file,
+			owner => "openldap",
+			group => "root",
+			content => "# Needed for the Makefile, to ensure there's at least 1 file here. Do not remove.\n",
+			require => File["/etc/ldap/slapd.db.d"];
+		"/etc/ldap/slapd.backend.d/bdb.conf":
+			source => "puppet://puppet/openldap/server/common/slapd.backend.d/bdb.conf",
+			ensure => file,
+			owner => "openldap",
+			group => "root",
+			mode => 644,
+			require => File["/etc/ldap/slapd.db.d"],
+			notify => Exec["update-slapd-conf"];
+		"/etc/ldap/slapd.db.d":
+			ensure => directory,
+			owner => "openldap",
+			group => "root",
+			mode => 750,
+			require => Package["slapd"];
+		"/etc/ldap/slapd.db.d/_dummy":
+			ensure => file,
+			owner => "openldap",
+			group => "root",
+			content => "# Needed for the Makefile, to ensure there's at least 1 file here. Do not remove.\n",
+			require => File["/etc/ldap/slapd.db.d"];
 		"/etc/ldap/create_slapd_conf":
-			source => "puppet://puppet/openldap/server/create_slapd_conf",
+			source => "puppet://puppet/openldap/server/common/create_slapd_conf",
 			owner => "root",
 			group => "root",
 			mode => 755,
-			require => File["/etc/ldap/slapd.conf.d"];
+			require => File["/etc/ldap/slapd.db.d"];
 		"/etc/ldap/Makefile":
-			source => "puppet://puppet/openldap/server/Makefile",
+			source => "puppet://puppet/openldap/server/common/Makefile",
 			owner => "root",
 			group => "root",
 			mode => 644,
 			require => File["/etc/ldap/create_slapd_conf"];
 	}
 
-	# Creates /etc/ldap/slapd_includes.conf, which does an include for all
-	# the other configuration files in /etc/ldap/slapd.conf.d.
+	# Creates /etc/ldap/slapd_includes.conf and /etc/ldap/slapd_dbs.conf,
+	# which include the configuration files in /etc/ldap/slapd.db.d and
+	# /etc/ldap/slapd.db.d respectively.
 	exec { "update-slapd-conf":
 		command => "/bin/sh -c 'cd /etc/ldap && make'",
 		refreshonly => true,

0455e9cdd9a14e76589e7e09e5c0a0ca9010308d
Bart Cortooms <bart@kumina.nl>
2009-08-20 15:19:25 +0200

    Remove "ensure => running" for Samba. We may not always want it running, e.g. with failover setups.
    darcs-hash:20090820131925-431e1-fe6dd8a0a86beafeecac1dbaa5a98f5c28abda3c.gz

--- samba/manifests/init.pp
+++ samba/manifests/init.pp
@@ -1,33 +1,27 @@
 class samba::common {
-	package {
-		"samba-common":
-			ensure => installed;
+	package { "samba-common":
+		ensure => installed;
 	}
 }
 
 class samba::server {
 	include samba::common
 
-	package {
-		"samba":
-			require => Package["samba-common"],
-			ensure => installed;
+	package { "samba":
+		require => Package["samba-common"],
+		ensure => installed;
 	}
 
-	service {
-		"samba":
-			subscribe => File["/etc/samba/smb.conf"],
-			pattern => "smbd",
-			ensure => running;
-			# TODO: FIX STATUS (Using Tim's patch?)
+	service { "samba":
+		subscribe => File["/etc/samba/smb.conf"],
+		pattern => "smbd",
 	}
 
-	file {
-		"/etc/samba/smb.conf":
-			source => "puppet://puppet/samba/samba/smb.conf",
-			mode => 644,
-			owner => "root",
-			group => "root",
-			require => Package["samba"];
+	file { "/etc/samba/smb.conf":
+		source => "puppet://puppet/samba/samba/smb.conf",
+		mode => 644,
+		owner => "root",
+		group => "root",
+		require => Package["samba"];
 	}
 }

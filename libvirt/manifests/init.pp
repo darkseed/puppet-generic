15f9290115fdcc85eb1dbff83b9d2b0cc642fe0a
Kees Meijs <kees@kumina.nl>
2010-01-13 14:34:19 +0100

    Make libvirt-bin more simple
    darcs-hash:20100113133419-6c6a4-faffb9da63ca7990b61c313d8c8924891eb8fcb2.gz

--- libvirt/manifests/init.pp
+++ libvirt/manifests/init.pp
@@ -1,27 +1,31 @@
 class libvirt {
-	package { "libvirt-bin":
-		ensure => installed,
+	package {
+		"libvirt-bin":
+			ensure => installed;
+		"libvirt-doc":
+			ensure => installed;
+		"netcat-openbsd":
+			ensure => installed;
 	}
 
-	service { "libvirt-bin":
-		hasrestart => true,
-		hasstatus => true,
-		require => Package["libvirt-bin"],
+	service {
+		"libvirt-bin":
+			hasrestart => true,
+			hasstatus => true,
+			require => Package["libvirt-bin"];
 	}
 
 	file {
-		"/etc/default/libvirt-bin":
-			source => "puppet://puppet/libvirt/default/libvirt-bin",
-			owner => "root",
-			group => "root",
-			mode => 644,
-			notify => Service["libvirt-bin"];
 		"/etc/libvirt/libvirtd.conf":
-			content => "puppet://puppet/libvirt/libvirt/libvirtd.conf",
 			owner => "root",
 			group => "root",
 			mode => 644,
 			require => Package["libvirt-bin"],
 			notify => Service["libvirt-bin"];
+		"/var/run/libvirt/libvirt-sock":
+			owner => "root",
+			group => "adm",
+			mode => 660,
+			require => Package["libvirt-bin"];
 	}
 }

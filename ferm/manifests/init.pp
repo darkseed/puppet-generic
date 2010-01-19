cfd3e1d4a7ffd5c69ac90bf3563497fc5f614194
Mike Huijerjans <mike@kumina.nl>
2010-01-19 10:17:34 +0100

    Simplify ferm module
    darcs-hash:20100119091734-9bc6d-cb23d357e5c54699c8b153f4e6de9374fc8f1298.gz

--- ferm/manifests/init.pp
+++ ferm/manifests/init.pp
@@ -1,48 +1,21 @@
 class ferm {
-	define rule($table="filter", $chain="INPUT", $interface=false,
-	            $outerface=false, $protocol=false, $saddr=false,
-		    $daddr=false, $sport=false, $dport=false, $action) {
-		file { "/etc/ferm/rules.d/$name.conf":
-			content => template("ferm/rule.conf"),
-			require => File["/etc/ferm/rules.d"];
-		}
+	package {
+		"ferm":
+			ensure => installed;
 	}
 
-	package { "ferm":
-		ensure => installed,
-	}
-
-	service { "ferm":
-		hasrestart => true,
-		subscribe => [File["/etc/ferm/ferm.conf"],
-		              File["/etc/ferm/rules.d"],
-		              File["/etc/ferm/vars.d"]]
+	service { 
+		"ferm":
+			hasrestart => true,
+			subscribe => File["/etc/ferm/ferm.conf"],
+			require => Package["ferm"];
 	}
 
 	file {
 		"/etc/ferm/ferm.conf":
-			source => "puppet://puppet/ferm/ferm.conf",
 			mode => 644,
 			owner => "root",
-			group => "root",
-			require => [File["/etc/ferm/vars.d"], File["/etc/ferm/rules.d"]];
-		"/etc/ferm/vars.d":
-			ensure => directory,
-			mode => 755,
-			owner => "root",
-			group => "root",
-			require => Package["ferm"];
-		"/etc/ferm/modules.d":
-			ensure => directory,
-			mode => 755,
-			owner => "root",
-			group => "root",
-			require => Package["ferm"];
-		"/etc/ferm/rules.d":
-			ensure => directory,
-			mode => 755,
-			owner => "root",
-			group => "root",
+			group => "adm",
 			require => Package["ferm"];
 	}
 }

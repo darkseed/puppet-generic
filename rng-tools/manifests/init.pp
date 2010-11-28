cb2d243f2ccb86affda4a2133152482177067732
Tim Stoop <tim@kumina.nl>
2010-11-28 01:56:17 +0100

    Add simple module that adds rng-tools to fill up entropy pool.
    darcs-hash:20101128005617-59231-4c89d0c0734b591b9f5415e0ed691d90440f896b.gz

---
+++ rng-tools/manifests/init.pp
@@ -0,0 +1,17 @@
+class rng-tools {
+	package { "rng-tools":
+		ensure => installed,
+	}
+
+	file { "/etc/default/rng-tools":
+		source  => "puppet:///modules/rng-tools/default/rng-tools",
+		notify  => Service["rng-tools"],
+		require => Package["rng-tools"],
+	}
+
+	service { "rng-tools":
+		ensure  => running,
+		pattern => "/usr/sbin/rngd",
+		require => File["/etc/default/rng-tools"],
+	}
+}

7ecc9f58e1bec483060cce160dd0faab04746701
Tim Stoop <tim@kumina.nl>
2010-12-02 17:34:22 +0100

    Add asterisk plugins to munin.
    darcs-hash:20101202163422-59231-1e0b846c2b3b8d9353d80d0eec2d86f373b898a3.gz

--- asterisk/manifests/init.pp
+++ asterisk/manifests/init.pp
@@ -11,6 +11,32 @@ class asterisk::server {
 			require => Package["asterisk"],
 			ensure => running;
 	}
+
+	# Simple define to ease creation of Munin plugin links
+	define munin_plugin {
+		file { "/etc/munin/plugins/$name":
+			ensure => link,
+			target => "/usr/local/share/munin/plugins/$name",
+		}
+	}
+
+	# Munin config
+	munin_plugin {
+		"asterisk_channels":;
+		"asterisk_channelstypes":;
+		"asterisk_codecs":;
+		"asterisk_console":;
+		"asterisk_iaxchannels":;
+		"asterisk_iaxlag":;
+		"asterisk_iaxpeers":;
+		"asterisk_meetme":;
+		"asterisk_meetmeusers":;
+		"asterisk_modules":;
+		"asterisk_sipchannels":;
+		"asterisk_sipobjects":;
+		"asterisk_sippeers":;
+		"asterisk_voicemail":;
+	}
 }
 
 class asterisk::zaptel-module-xen {

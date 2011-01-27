08abc5df85aabbc5810f8598600f5e504ed4deff
Tim Stoop <tim@kumina.nl>
2011-01-27 12:32:58 +0100

    Make the reload use initscript.
    darcs-hash:20110127113258-59231-8bc432e67b30b727a70cfebd5a51ad22f154e42b.gz

--- ferm/manifests/init.pp
+++ ferm/manifests/init.pp
@@ -1,22 +1,19 @@
 class ferm {
-	package {
-		"ferm":
-			ensure => installed;
+	package { "ferm":
+		ensure => installed;
 	}
 
-	exec { 
-		"reload-ferm":
-			command => "/usr/sbin/ferm /etc/ferm/ferm.conf",
-			subscribe => File["/etc/ferm/ferm.conf"],
-			refreshonly => true;
+	exec { "reload-ferm":
+		command     => "/etc/init.d/ferm reload",
+		subscribe   => File["/etc/ferm/ferm.conf"],
+		refreshonly => true;
 	}
 
-	file {
-		"/etc/ferm/ferm.conf":
-			ensure  => file,
-			mode    => 644,
-			owner   => "root",
-			group   => "adm",
-			require => Package["ferm"];
+	file { "/etc/ferm/ferm.conf":
+		ensure  => file,
+		mode    => 644,
+		owner   => "root",
+		group   => "adm",
+		require => Package["ferm"];
 	}
 }

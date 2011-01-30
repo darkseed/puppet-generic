5c814aae2edcb92188332d1c291e7f00628d5a26
Tim Stoop <tim@kumina.nl>
2011-01-30 21:17:18 +0100

    New module for hetzner failover stuff.
    darcs-hash:20110130201718-59231-09b482e87f1bbc3ef19672efd6199e2ccff79c5f.gz

---
+++ hetzner/files/parse-hetzner-json.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+import simplejson as json, urllib2, ConfigParser, sys, urllib
+from optparse import OptionParser
+
+# Parse the commandline options
+parser = OptionParser()
+parser.add_option("-l", "--list",
+                  action="store_true", dest="list_action", default=False,
+                  help="get current settings")
+parser.add_option("-g", "--get",
+                  action="store_true", dest="get_action", default=False,
+                  help="check whether the failover ip is pointing to us or not")
+parser.add_option("-s", "--set",
+                  action="store_true", dest="set_action", default=False,
+                  help="set the failover IP to this host")
+parser.add_option("-i", "--ip",
+                  dest="failover_ip", default="0",
+                  help="the failover ip to manipulate", metavar="FAILOVER-IP")
+parser.add_option("-c", "--config",
+		  dest="configfile", default="/etc/hetzner.cfg",
+		  help="the location of the config file, defaults to /etc/hetzner.cfg", metavar="FILE")
+
+(options, args) = parser.parse_args()
+
+# Parse the configfile
+config = ConfigParser.SafeConfigParser()
+config.read(options.configfile)
+section = config.sections()[0]
+
+# Create the auth handler
+pass_handler = urllib2.HTTPPasswordMgrWithDefaultRealm()
+pass_handler.add_password(realm=None,
+                          uri="https://robot-ws.your-server.de",
+                          user=config.get(section,"user"),
+			  passwd=config.get(section,"pass"))
+auth_handler = urllib2.HTTPBasicAuthHandler(pass_handler)
+opener = urllib2.build_opener(auth_handler)
+urllib2.install_opener(opener)
+
+def list_failover_ips():
+	value = urllib2.urlopen("https://robot-ws.your-server.de/failover")
+	# Make from the json a python object
+	data = json.load(value)
+	# Get the ips
+	ips = []
+	for i in data:
+		ips.append(i["failover"]["ip"])
+	return ips
+
+def get_failover_ip(ip):
+	url = "https://robot-ws.your-server.de/failover/"+ip
+	value = urllib2.urlopen(url)
+	data = json.load(value)
+	return data["failover"]["active_server_ip"]
+
+def set_failover_ip(ip):
+	url = "https://robot-ws.your-server.de/failover/"+ip
+	data = urllib.urlencode({"active_server_ip":config.get(section,"local_ip")})
+	value = urllib2.urlopen(url,data)
+	data = json.load(value)
+	return data["failover"]["active_server_ip"]
+
+#Decide what we want to do
+if options.list_action:
+	if options.failover_ip == "0":
+		for x in list_failover_ips():
+			print x
+		sys.exit(0)
+	else:
+		if options.failover_ip in list_failover_ips():
+			sys.exit(0)
+	sys.exit(1)
+
+elif options.get_action:
+	# Check if the option is set
+	if options.failover_ip == "0":
+		raise ValueError, "Need to set the failover IP."
+	# Check if the failover_ip is actually one we can use
+	if not options.failover_ip in list_failover_ips():
+		raise ValueError, "This failover IP is not assigned to the user in the config file."
+	# Get the active ip for the failover ip
+	active_ip = get_failover_ip(options.failover_ip)
+	# Does the ip point to us?
+	if config.get(section, "local_ip") == active_ip:
+		print "Assigned to us."
+		sys.exit(0)
+	else:
+		print "Not assigned to us, but to %s." % (active_ip)
+		sys.exit(1)
+
+elif options.set_action:
+	# Check if the option is set
+	if options.failover_ip == "0":
+		raise ValueError, "Need to set the failover IP."
+	# Check if the failover_ip is actually one we can use
+	if not options.failover_ip in list_failover_ips():
+		raise ValueError, "This failover IP is not assigned to the user in the config file."
+	if get_failover_ip(options.failover_ip) == config.get(section,"local_ip"):
+		print "Already set."
+		sys.exit(0)
+	set_failover_ip(options.failover_ip)
+	print "Set."
+	sys.exit(0)
---
+++ hetzner/manifests/init.pp
@@ -0,0 +1,29 @@
+# Copyright (C) 2010 Kumina bv, Tim Stoop <tim@kumina.nl>
+# This works is published under the Creative Commons Attribution-Share 
+# Alike 3.0 Unported license - http://creativecommons.org/licenses/by-sa/3.0/
+# See LICENSE for the full legal text.
+
+class hetzner::failover_ip {
+	package { "python-simplejson":
+		ensure => latest,
+	}
+
+	file { "/usr/local/sbin/parse-hetzner-json.py":
+		source => "puppet:///modules/hetzner/parse-hetzner-json.py",
+		owner  => "root",
+		group  => "root",
+		mode   => 755,
+	}
+
+	file { "/usr/local/lib/hetzner":
+		ensure => directory,
+	}
+
+	file { "/usr/local/lib/hetzner/hetzner-failover-ip":
+		source => "puppet:///modules/hetzner/hetzner-failover-ip",
+		owner  => "root",
+		group  => "root",
+		mode   => 755,
+	}
+
+}
